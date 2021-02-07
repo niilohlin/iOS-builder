81d9b71f9ea5ab3fc2db4d46b210abef28de7271
Niil Öhlin <niilohlin@gmail.com>
2021-02-07 17:09:28 +0100

    add bundler role

--- ruby/files/export_path.sh
+++ ruby/files/export_path.sh
@@ -1,15 +1,12 @@
-
-
-touch $HOME/.profile
+#!/bin/sh
 
 if ! grep -q "\$HOME/.rbenv/bin" $HOME/.zshrc; then
     echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $HOME/.profile
+    echo 'changed'
 fi
 
-touch $HOME/.zshrc
 if ! grep -q "rbenv init -" $HOME/.zshrc; then
     echo 'eval "$(rbenv init -)"' >> $HOME/.zshrc
+    echo 'changed'
 fi
 
-source $HOME/.profile
-source $HOME/.zshrc
